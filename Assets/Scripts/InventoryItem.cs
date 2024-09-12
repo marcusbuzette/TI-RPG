bb067c35006c362b89c205180fe8fef70ba5f891
Wakabayashiii <igorbraga1708@gmail.com>
2024-09-12 19:45:55 -0300

    Inventário
    Sistema de inventário com adição e remoção de itens

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+ [Serializable]
+public class InventoryItem
+{
+    public InventoryItemData data {get; private set;}
+    public int stackSize{get; private set;}
+
+    public InventoryItem(InventoryItemData source)
+    {
+        data = source;
+        AddToStack();
+    }
+
+    
+   public void AddToStack()
+   {
+    stackSize++;
+   }
+
+   public void RemoveFromStack()
+   {
+    stackSize--;
+   }
+}
