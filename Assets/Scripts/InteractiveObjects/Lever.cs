be8993725350bfa02eb90b20ee9b91dea09dd51b
PEDRO ALEX <132845901+PadekZ@users.noreply.github.com>
2025-04-23 19:23:12 -0300

    Improved Chest and Lever Scripts
    Now both scripts inherit from an interface.

---
+++ Assets/Scripts/InteractiveObjects/Lever.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour, IInteractiveObjects
+{
+    private bool goingTo = false;
+    private GridPosition targetToUnit;
+    private Unit currentUnit;
+
+    private void Start() {
+        GetFowardGridObject();
+    }
+
+    private void Update() {
+        if (goingTo) {
+            if (currentUnit.GetGridPosition() == targetToUnit) {
+                LevelGrid.Instance.OnGameModeChanged -= UnitStopGoingTo;
+                PathFinding.Instance.OnRecalculatedpath -= UnitStopGoingTo;
+
+                Debug.Log("Abrir Porta");
+
+                goingTo = false;
+            }
+        }
+    }
+
+    public void GetFowardGridObject() {
+        Vector3 pos = (transform.forward * 2) + transform.position;
+        targetToUnit = LevelGrid.Instance.GetGridPosition(pos);
+        Debug.Log(targetToUnit);
+    }
+
+    public void MoveUnitToGridPostion(Unit unit) {
+        goingTo = true;
+        currentUnit = unit;
+
+        UnitActionSystem.Instance.MoveUnitToGridPosition(currentUnit, targetToUnit);
+        LevelGrid.Instance.OnGameModeChanged += UnitStopGoingTo;
+        PathFinding.Instance.OnRecalculatedpath += UnitStopGoingTo;
+    }
+
+    public void UnitStopGoingTo(object sender, EventArgs e) {
+        LevelGrid.Instance.OnGameModeChanged -= UnitStopGoingTo;
+        PathFinding.Instance.OnRecalculatedpath -= UnitStopGoingTo;
+
+        Debug.Log("PAROU DE IR");
+
+        goingTo = false;
+        currentUnit = null;
+    }
+
+    public void UnitStopGoingTo() {
+        LevelGrid.Instance.OnGameModeChanged -= UnitStopGoingTo;
+        PathFinding.Instance.OnRecalculatedpath -= UnitStopGoingTo;
+
+        goingTo = false;
+        currentUnit = null;
+    }
+}
