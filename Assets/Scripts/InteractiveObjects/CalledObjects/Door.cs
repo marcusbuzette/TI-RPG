14cc34690a56848226fdacd5affa99b6d9fb7070
PEDRO ALEX <132845901+PadekZ@users.noreply.github.com>
2025-04-24 21:30:34 -0300

    Lever and Bridge in Tutorial Scene

--- Assets/Scripts/InteractiveObjects/CalledObjects/Door.cs
+++ Assets/Scripts/InteractiveObjects/CalledObjects/Door.cs
@@ -18,18 +18,43 @@ public class Door : MonoBehaviour, ICalledObject {
         PathFinding.Instance.SetNodeIsWalkable(transform.position ,isWalkable);
     }
 
-    private IEnumerator GoToPosition(float time) {
+    /*private IEnumerator GoToPosition(float time) {
         Vector3 startingPos = transform.position;
         Vector3 finalPos = targetTransform.position;
-
+        Debug.Log("Entrou");
         float elapsedTime = 0;
 
         while (elapsedTime < time) {
+            Debug.Log("indo");
             transform.position = Vector3.Lerp(startingPos, finalPos, (elapsedTime / time));
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
         yield return null;
+    }*/
+
+    private IEnumerator GoToPosition(float time) {
+        Vector3 startingPos = transform.position;
+        Vector3 finalPos = targetTransform.position;
+
+        Quaternion startingRot = transform.rotation;
+        Quaternion finalRot = targetTransform.rotation;
+
+        float elapsedTime = 0;
+
+        while (elapsedTime < time) {
+            float t = elapsedTime / time;
+            transform.position = Vector3.Lerp(startingPos, finalPos, t);
+            transform.rotation = Quaternion.Lerp(startingRot, finalRot, t);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // Garante que a posição e rotação finais sejam exatamente as do target
+        transform.position = finalPos;
+        transform.rotation = finalRot;
     }
+
 }
